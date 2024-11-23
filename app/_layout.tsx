427f1f0a7c9246131bb9e13b33a6d901fdd38a21
Salal Saleem <salalsaleem2420@gmail.com>
2024-11-23 18:53:49 +0500

    updated

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,36 +1,50 @@
 import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
 import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
-import { StatusBar } from 'expo-status-bar';
 import { useEffect } from 'react';
 import 'react-native-reanimated';
 
-import { useColorScheme } from '@/hooks/useColorScheme';
-import { Text, View } from 'react-native';
+import { Text, View , StyleSheet,Image } from 'react-native';
+import { MaterialIcons } from '@expo/vector-icons';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
-// Prevent the splash screen from auto-hiding before asset loading is complete.
-SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
-  const colorScheme = useColorScheme();
-  const [loaded] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
-  });
-
-  useEffect(() => {
-    if (loaded) {
-      SplashScreen.hideAsync();
-    }
-  }, [loaded]);
-
-  if (!loaded) {
-    return null;
-  }
-
+  
   return (
-    <View style={{height:300, flex:1 , backgroundColor:"black" , flexDirection:"row" , justifyContent:"center"}}>
-     <Text style={{color:"white"}}>Hello salal</Text>
-     </View>
+    <SafeAreaView style={{flex:1}}>
+<View style={{flexDirection:"row",alignItems:"center", justifyContent:"space-between",backgroundColor:"green",height:50}}>
+     <MaterialIcons name="density-medium" size={24} color={"white"}></MaterialIcons>
+      <Text style={styles.logo}>Bykea </Text>
+      <MaterialIcons name="search" size={24} color={"white"}></MaterialIcons>
+      </View>
+      <View style={{backgroundColor:"orange", flex:1}}>
+        <View style={{flex:2.5}}>
+          <Image 
+          style={styles.img}
+           source={require("../assets/images/banner.jpg")}
+          />
+        </View>
+        <View style={{flex:0.5 , backgroundColor:"white"}}>
+          <MaterialIcons name="fastfood"></MaterialIcons>
+        </View>
+      </View>
+      <View style={{backgroundColor:"red",flex:1.5}}></View>
+      </SafeAreaView>
   );
 }
+
+
+
+const styles = StyleSheet.create({
+  logo:{
+    fontSize:21,
+    fontWeight:"bold",
+    color:"white"
+  },
+  img:{
+    resizeMode:"stretch",
+    width:"100%",
+    height:"100%"
+  }
+})
